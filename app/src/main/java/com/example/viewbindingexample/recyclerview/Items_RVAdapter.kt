a415093355f9c682fc0cd4b76f0a7213f6040b20
Brian Donnoe <dunow33@hotmail.com>
2021-10-24 23:23:58 -0400

    original files

--- app/src/main/java/com/example/viewbindingexample/recyclerview/Items_RVAdapter.kt
+++ app/src/main/java/com/example/viewbindingexample/recyclerview/Items_RVAdapter.kt
@@ -5,24 +5,44 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.viewbindingexample.databinding.ItemRowBinding
 
-class Items_RVAdapter(private var itemsCells: ArrayList<String?>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+//class Items_RVAdapter(private var itemsCells: ArrayList<String?>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+//
+//    override fun getItemCount(): Int {
+//        return itemsCells.size
+//    }
+//
+//    class ItemViewHolder(var viewBinding: ItemRowBinding) : RecyclerView.ViewHolder(viewBinding.root)
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+//        val binding = ItemRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+//        return ItemViewHolder(binding)
+//    }
+//
+//    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+//        val itemViewHolder = holder as ItemViewHolder
+//        itemViewHolder.viewBinding.itemtextview.text = itemsCells[position]
+//    }
+//}
+
+
+class Items_RVAdapter(
+    private var itemsCells: ArrayList<String?>
+) : RecyclerView.Adapter<Items_RVAdapter.ItemViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val binding = ItemRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val itemViewHolder = holder as ItemViewHolder
+        itemViewHolder.binding.itemtextview.text = itemsCells[position]
+    }
 
     override fun getItemCount(): Int {
         return itemsCells.size
     }
 
-    class ItemViewHolder(var viewBinding: ItemRowBinding) : RecyclerView.ViewHolder(viewBinding.root)
-
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val binding = ItemRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ItemViewHolder(binding)
-    }
-
-
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val itemViewHolder = holder as ItemViewHolder
-        itemViewHolder.viewBinding.itemtextview.text = itemsCells[position]
-    }
+    class ItemViewHolder(var binding: ItemRowBinding) : RecyclerView.ViewHolder(binding.root)
 }
